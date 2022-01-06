4a1cd08f46010acca753ec4d1850a5dd2dc2bff2
raph <raphitto.rn@gmail.com>
2022-01-06 15:46:22 +0300

    initial commit boilerplate code for landing site

---
+++ index.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="main.css">
+    <script src="main.js"></script>
+</head>
+<body>
+    
+
+
+<h1>Campus wifi</h1>
+
+<table class="rtable">
+  <thead>
+    <tr>
+      <th>Wifi name</th>
+      <th>strength</th>
+      <th>traffic</td>
+      <th>location</th>
+      <th>status</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>eduroam lib</td>
+      <td>9,562</td>
+      <td>68.81%</td>
+      <td>7,895</td>
+      <td>01:07</td>
+    </tr>
+    <tr>
+      <td>eduroam cherry park</td>
+      <td>2,403</td>
+      <td>17.29%</td>
+      <td>2,046</td>
+      <td>00:59</td>
+    </tr>
+    <tr>
+      <td>eduroam hall 7</td>
+      <td>1,089</td>
+      <td>2.63%</td>
+      <td>904</td>
+      <td>00:59</td>
+    </tr>
+    <tr>
+      <td>Internet from the moon</td>
+      <td>366</td>
+      <td>2.63%</td>
+      <td>333</td>
+      <td>01:01</td>
+    </tr>
+    <tr>
+      <td>jkuat wireless test</td>
+      <td>162</td>
+      <td>1.17%</td>
+      <td>112</td>
+      <td>00:58</td>
+    </tr>
+    <tr>
+      <td>eduroam pavilion</td>
+      <td>103</td>
+      <td>0.74%</td>
+      <td>87</td>
+      <td>01:22</td>
+    </tr>
+    <tr>
+      <td>scc 105</td>
+      <td>98</td>
+      <td>0.71%</td>
+      <td>69</td>
+      <td>01:18</td>
+    </tr>
+  </tbody>
+</table>
+<!-- 
+<p><a target="_blank" href="http://dbushell.com/2016/03/04/css-only-responsive-tables/">See my blog article for more information</a></p>
+
+<h3>Optional adjustments to suit your content:</h3>
+
+<ul>
+  <li><code>white-space: nowrap</code> is optional on the <code>&lt;table&gt;</code> element</li>
+  <li>Add <code>min-width</code> to cells to better space content</li>
+  <li>Wrap the flip CSS in a media query?</li>
+</ul>
+
+<p><strong>Please note:</strong> this CodePen has Autoprefixer enabled. You may need vendor prefixes for full CSS gradient and Flexbox support.</p>
+
+<p><strong>Supports:</strong> IOS7+, Android 4.3+</p>
+
+<p><strong>by: <a target="_blank" href="http://dbushell.com/">David Bushell</a></strong>
+ -->
+
+</body>
+</html>
